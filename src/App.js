5b8766bc833c45c9fb373b27c432974ba9a3935d
Pav125 <devipavan824@gmail.com>
2024-05-11 17:00:53 +0530

    Crated logic for bg change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+// import ReactDOM from 'react-dom'
 
-function App() {
+import './App.css'
+
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='bg'>
+        <h1>Pick color to change BackGround</h1>
+        <input type='color' id='clr' onChange={(e)=>{
+          document.body.style.background = e.target.value
+        }} />
+      </div>
+    </>
+  )
 }
 
 export default App;
